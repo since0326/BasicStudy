f94ee7d507ca82ba2590fe1252030b2b65c6306d
jaehyeok.Kim <kjh960326@naver.com>
2022-08-14 20:11:29 +0900

    열혈C++ Day1

---
+++ Passion/Passion/FunctionOverloading.cpp
@@ -0,0 +1,25 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+void MyFunc(){
+	cout << "Myfunc(void) called" << endl;
+}
+
+void MyFunc(char c){
+	cout << "Myfunc(char c) called" << endl;
+}
+
+void MyFunc(int a, int b){
+	cout << "Myfunc(int a, int b) called" << endl;
+}
+
+int main(void){
+	MyFunc();
+	MyFunc('A');
+	MyFunc(12,13);
+	
+	return 0;
+}
+*/
---
+++ Passion/Passion/Q1-1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+//1번 
+int main(void){
+	int num;
+	int result = 0;
+	
+	for(int i=1; i<=5; i++){
+		cout << i <<"번째 정수 입력 : ";
+		cin >> num;
+		result += num;
+	}
+	
+	cout << "입력하신 정수들의 합 : " << result << endl;
+	
+	return 0;
+}
+*/
+
+/*
+//2번
+int main(void){
+	char name[100];
+	char pNum[100];
+	
+	cout << "이름을 입력하세요 : ";
+	cin >> name;
+	
+	cout << "전화번호를 입력하세요 : ";
+	cin >> pNum;
+	
+	cout << "이름은 " << name << ", 전화번호는 " << pNum << endl;
+	
+	return 0;
+} 
+*/
+
+/*
+//3번
+int main(void){
+	int num;
+	
+	cout << "출력하실 단을 입력하세요 : ";
+	cin >> num;
+	
+	for(int i=1; i<=9; i++){
+		cout << num << " x " << i << " = "  << num*i << endl;
+	}
+	
+	return 0;
+} 
+*/
+
+/*
+//4번
+int main(void){
+	int pay;
+	
+	while(1){
+		cout << "판매 금액을 만원 단위로 입력(-1 to end) : ";
+		cin >> pay;
+		
+		if(pay==-1){
+			break;
+		}
+		
+		cout << "이번달 급여 : " << 50+(pay*0.12) << "만원" <<endl; 
+	}
+	
+	cout << "프로그램 종료" << endl;
+		
+	return 0;
+} 
+*/
